a3ea42ec0c5b7126768ef21ccbec482c5b7c54e1
Jakob Heuser <jakob@codedrift.com>
2024-11-08 17:32:44 -0800

    fix: Fixes rust code run path

--- rs-lua/src/lib.rs
+++ rs-lua/src/lib.rs
@@ -1,9 +1,9 @@
 use extism_pdk::*;
-use mlua::{chunk, FromLua, Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Value, Variadic};
+use mlua::*;
 
 #[plugin_fn]
 pub fn execLua(code: String) -> FnResult<String> {
     let lua = Lua::new();
-    let result = lua.load("1 + 1").eval::<String>()?,
+    let result = lua.load(code).eval::<String>()?,
     Ok(result.unwrap().to_string())
 }
